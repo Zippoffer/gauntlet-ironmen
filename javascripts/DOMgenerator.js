8145db9cf2a2e102f8ceb9816a5b2bf61dffc27b
Marshall Friskics-Warren <wmfwarren@gmail.com>
2016-07-06 17:19:20 -0500

    Added event listeners to check for picked class and weapons

--- javascripts/DOMgenerator.js
+++ javascripts/DOMgenerator.js
@@ -1,38 +1,42 @@
 "use strict";
 
-var barWars = require("./barWars.js");
-var classes = require("./classes.js");
-var attacks = require("./attacks.js");
-var specials = require("./specialAbilities.js"); //for future-proofing
+	var barWars = require("./barWars.js");
+	var classes = require("./classes.js");
+	var attacks = require("./attacks.js");
+	var specials = require("./specialAbilities.js"); //for future-proofing
 
 
-/////***Event Listeners***\\\\\
+$(document).ready(function(){
+	/////***Event Listeners***\\\\\
+	$("#patronClasses").change(barWars.identifyPatronClass);
+	$("#staffClasses").change(barWars.identifyStaffClass);
+	$("#patronAttacks").change(barWars.identifyPatronAttack);
+	$("#staffAttacks").change(barWars.identifyStaffAttack);
+	/////***DropDown Populating Functions***\\\\\
+	function populatePatronClasses(){
+		for(let i = 0; i < classes.patronClassArray.length; i++){
+			let currentClass = classes.patronClassArray[i];
+			let currentClassName = currentClass.name; 
+			let currentClassDisplayName = currentClassName.replace(/_/g, " ");
 
-
-/////***DropDown Populating Functions***\\\\\
-function populatePatronClasses(){
-	for(let i = 0; i < classes.patronClassArray.length; i++){
-		let currentClass = classes.patronClassArray[i];
-		let currentClassName = currentClass.name; 
-		let currentClassDisplayName = currentClassName.replace(/_/g, " ");
-
-		$("#patronClasses").append(`<option value="${currentClassName}">${currentClassDisplayName}</option>`);
+			$("#patronClasses").append(`<option value="${currentClassName}">${currentClassDisplayName}</option>`);
+		}
 	}
-}
 
-function populatePatronAttacks(){
-	for(let i = 0; i < attacks.patronAttacksArray.length; i++){
-		let currentAttack = attacks.patronAttacksArray[i];
-		let currentAttackName = currentAttack.name; 
-		let currentAttackDisplayName = currentAttackName.replace(/_/g, " ");
+	function populatePatronAttacks(){
+		for(let i = 0; i < attacks.patronAttacksArray.length; i++){
+			let currentAttack = attacks.patronAttacksArray[i];
+			let currentAttackName = currentAttack.name; 
+			let currentAttackDisplayName = currentAttackName.replace(/_/g, " ");
 
-		$("#patronAttacks").append(`<option value="${currentAttackName}">${currentAttackDisplayName}</option>`);
+			$("#patronAttacks").append(`<option value="${currentAttackName}">${currentAttackDisplayName}</option>`);
+		}
 	}
-}
-////////Add a populateStaffClasses() ::: Dont forget to call functions
-////////Add a populateStaffAttacks() ::: Dont Forget to call functions
+	////////Add a populateStaffClasses() ::: Dont forget to call functions
+	////////Add a populateStaffAttacks() ::: Dont Forget to call functions
 
 
-/////***Call Functions to populate DOM***\\\\\
-populatePatronClasses();
-populatePatronAttacks();
+	/////***Call Functions to populate DOM***\\\\\
+	populatePatronClasses();
+	populatePatronAttacks();
+});

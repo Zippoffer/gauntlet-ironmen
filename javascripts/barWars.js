bcfc5c6e1dabcac48b8e201d93eaa5d2923f2403
Marshall Friskics-Warren <wmfwarren@gmail.com>
2016-07-06 17:20:09 -0500

    added functions to select the patron and staff vars

--- javascripts/barWars.js
+++ javascripts/barWars.js
@@ -31,6 +31,42 @@ var patronTurn = true;
 ////function for rolling damage.
 //changing button's disabled status on turn change
 
+function identifyPatronClass(event){
+	for(let i = 0; i < classes.patronClassArray.length; i++){
+		let currentClass = classes.patronClassArray[i];
+		if($("#patronClasses").val()[0] === currentClass.name){
+			patronClass = currentClass;
+			// console.log("Patron Class", patronClass);
+		}
+	}
+}
+function identifyStaffClass(event){
+	for(let i = 0; i < classes.staffClassArray.length; i++){
+		let currentClass = classes.staffClassArray[i];
+		if($("#staffClasses").val()[0] === currentClass.name){
+			staffClass = currentClass;
+			// console.log("Patron Class", patronClass);
+		}
+	}
+}
+function identifyPatronAttack(event){
+	for(let i = 0; i < attacks.patronAttackArray.length; i++){
+		let currentAttack = attacks.patronAttackArray[i];
+		if($("#patronAttacks").val()[0] === currentAttack.name){
+			patronAttack = currentAttack;
+			// console.log("Patron Class", patronClass);
+		}
+	}
+}
+function identifyStaffAttack(event){
+	for(let i = 0; i < attacks.staffAttackArray.length; i++){
+		let currentAttack = attacks.staffAttackArray[i];
+		if($("#staffAttacks").val()[0] === currentAttack.name){
+			staffAttack = currentAttack;
+			// console.log("Patron Class", patronClass);
+		}
+	}
+}
 
 /////***Exports for Browserify***\\\\\
-module.exports = {};
+module.exports = {identifyPatronClass};
